c6e86bb5f106376733d444fbeea510c65965a96a
afrozaft <40798386+afrozaft@users.noreply.github.com>
2025-01-07 23:30:11 +0530

    modify the list-employee table

--- pages/list-employee.php
+++ pages/list-employee.php
@@ -13,33 +13,41 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-10">
-                    <h2>List Employee</h2>
-                <table class="table" id="tbl-employee">
-                <thead>
-                <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th>
-                    <th>Email</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>John</td>
-                    <td>Doe</td>
-                    <td>john@example.com</td>
-                </tr>
-                <tr>
-                    <td>Mary</td>
-                    <td>Moe</td>
-                    <td>mary@example.com</td>
-                </tr>
-                <tr>
-                    <td>July</td>
-                    <td>Dooley</td>
-                    <td>july@example.com</td>
-                </tr>
-                </tbody>
-            </table>
+            <h2>List Employee</h2>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">List of All Employee</div>
+                    <div class="panel-body">
+                        <table class="table" id="tbl-employee">
+                        <thead>
+                        <tr>
+                            <th>#ID</th>
+                            <th>#Name</th>
+                            <th>#Email</th>
+                            <th>#Phone No.</th>
+                            <th>#Gender</th>
+                            <th>#Designation</th>
+                            <th>#Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>John Doe</td>
+                            <td>john@example.com</td>
+                            <td>9874563216</td>
+                            <td>Male</td>
+                            <td>Wordpress Developer</td>
+                            <td>
+                                <a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+                                <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+                                <a href="javascript:void(0)" class="btn btn-info">View</a>
+                            </td>
+                        </tr>
+                       
+                        </tbody>
+                        </table>
+                        </div>
+                    </div>   
         </div>
     </div>
 </div>
