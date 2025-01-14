5f43835b22f228a9b1e9253438ea6890bdaf610d
afrozaft <40798386+afrozaft@users.noreply.github.com>
2025-01-14 10:15:52 +0530

    fetch query added to get the list of employee in the list-employee page

--- pages/list-employee.php
+++ pages/list-employee.php
@@ -1,4 +1,9 @@
+<?php 
+ global $wpdb;
 
+$employees = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}ems_form_data", ARRAY_A);
+
+?>
 
 <div class="container">
     <div class="row">
@@ -8,36 +13,47 @@
                     <div class="panel-heading">List of All Employee</div>
                     <div class="panel-body">
                         <table class="table" id="tbl-employee">
-                        <thead>
-                        <tr>
-                            <th># ID</th>
-                            <th># Name</th>
-                            <th># Email</th>
-                            <th># Phone No.</th>
-                            <th># Gender</th>
-                            <th># Designation</th>
-                            <th># Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>John Doe</td>
-                            <td>john@example.com</td>
-                            <td>9874563216</td>
-                            <td>Male</td>
-                            <td>Wordpress Developer</td>
-                            <td>
-                                <a href="javascript:void(0)" class="btn btn-warning">Edit</a>
-                                <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
-                                <a href="javascript:void(0)" class="btn btn-info">View</a>
-                            </td>
-                        </tr>
+                            <thead>
+                                <tr>
+                                    <th># ID</th>
+                                    <th># Name</th>
+                                    <th># Email</th>
+                                    <th># Phone No.</th>
+                                    <th># Gender</th>
+                                    <th># Designation</th>
+                                    <th># Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                   if(count($employees) > 0){
+                                    foreach($employees as $employee){
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $employee['id'] ?></td>
+                                            <td><?php echo $employee['name'] ?></td>
+                                            <td><?php echo $employee['email'] ?></td>
+                                            <td><?php echo $employee['phoneNo'] ?></td>
+                                            <td><?php echo ucfirst($employee['gender']) ?></td>
+                                            <td><?php echo $employee['designation'] ?></td>
+                                            <td>
+                                                <a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+                                                <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+                                                <a href="javascript:void(0)" class="btn btn-info">View</a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                   }else{
+                                        echo "No Employee Found!";
+                                   }
+                                ?>
+                                
                        
-                        </tbody>
+                            </tbody>
                         </table>
-                        </div>
-                    </div>   
+                    </div>
+                </div>   
         </div>
     </div>
 </div>
